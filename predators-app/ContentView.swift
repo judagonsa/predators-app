88ede48f5f642b8a5757762580faf788b179c9f7
judagonsa <judagonsa8@gmail.com>
2026-02-23 18:02:41 -0500

    chore: show list of predators

--- predators-app/ContentView.swift
+++ predators-app/ContentView.swift
@@ -11,29 +11,47 @@ struct ContentView: View {
     
     let predator = Predators()
     
-    var body: some View {
-        List(predator.apexPredators) { predator in
-            HStack {
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .red, radius: 1)
-                
-                VStack (alignment: .leading) {
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 15)
-                        .padding(.vertical, 5)
-                        .background(predator.type.backgroundColor)
-                        .cornerRadius(15)
-                }
-                
+    @State var searchText = ""
+    
+    var filteredPredators: [ApexPredator] {
+        if searchText.isEmpty {
+            return predator.apexPredators
+        }else {
+            return predator.apexPredators.filter { predator in
+                predator.name.localizedStandardContains(searchText)
             }
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List(filteredPredators) { predator in
+                HStack {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .shadow(color: .red, radius: 1)
+                    
+                    VStack (alignment: .leading) {
+                        Text(predator.name)
+                            .fontWeight(.bold)
+                        
+                        Text(predator.type.rawValue.capitalized)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                            .background(predator.type.backgroundColor)
+                            .cornerRadius(15)
+                    }
+                    
+                }
+            }
+            .navigationTitle("Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
         }
     }
 }
